820998549446979df4237d79640daa9802c94116
AazimAnish <aazimansh1@gmail.com>
2023-04-16 18:28:59 +0530

    store data on smart contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,27 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
+    mapping(address => uint256) public wavers;
+
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        wavers[msg.sender] += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
+    function getWavesByAddress(address addr) public view returns (uint256) {
+        console.log("%s has %d waves!", addr, wavers[addr]);
+        return wavers[addr];
     }
 }
